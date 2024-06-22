c6d51c8a70f0c1a9c2d4f6770332fb25ae450fd2
mhmadamrii <interceptorghost4@gmail.com>
2024-06-22 14:50:40 +0700

    init: initializing a basic app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+import Homepage from '@/components/Homepage'
+
+function App() {
+  return (
+    <>
+      <Homepage />
+      <span>Hello world</span>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,9 @@
+import { Button } from './ui/button';
+
+export default function Homepage() {
+  return (
+    <div>
+      <Button>Hello world</Button>
+    </div>
+  );
+}
